37bd6811bed1416cc57bbde2a15a7da286d01ee3
Jan Halfar <jan@bestbytes.com>
2021-03-02 21:42:51 +0100

    removed some cobra boilerplate

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,8 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/foomo/webgrapple/server"
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
 
@@ -34,32 +30,6 @@ func Execute() error {
 func init() {
 	logger, _ = zap.NewProduction()
 	defer logger.Sync()
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	//	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-
 	rootCmd.AddCommand(reverseProxyCmd)
 	rootCmd.AddCommand(clientNPMCmd)
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cobra")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
