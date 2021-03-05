6facd5acb8f48dbdca37b33135fd47db8dc4dc97
Jan Halfar <jan@bestbytes.com>
2021-03-05 16:50:50 +0100

    interface update

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/foomo/webgrapple"
 	"github.com/foomo/webgrapple/server"
@@ -11,7 +12,7 @@ import (
 
 func main() {
 	logger := utils.GetLogger()
-	server.MiddlewareCreator = func(services server.ServiceMap) (server.Middleware, error) {
+	server.MiddlewareCreator = func(services server.ServiceMap, backendURL *url.URL) (server.Middleware, error) {
 		logger.Info("new configuration")
 		return func(next http.HandlerFunc) http.HandlerFunc {
 			return func(w http.ResponseWriter, r *http.Request) {
