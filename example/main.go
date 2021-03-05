73d4ab7a0032d680bf7968bddd3df68e0fa280f8
Jan Halfar <jan@bestbytes.com>
2021-03-05 09:27:17 +0100

    an empty example

---
+++ example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/foomo/webgrapple"
+	"github.com/foomo/webgrapple/server"
+	"github.com/foomo/webgrapple/utils"
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger := utils.GetLogger()
+	server.MiddlewareCreator = func(services server.ServiceMap) (server.Middleware, error) {
+		logger.Info("new configuration")
+		return func(next http.HandlerFunc) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+
+			}
+		}, nil
+	}
+	errExecute := webgrapple.Command.Execute()
+	if errExecute != nil {
+		utils.GetLogger().Error("execution error", zap.Error(errExecute))
+	}
+}
